ccfba5ced97d70d0a02536a7adf45a5489ba4a45
Yao Wen <yaowen7902@gmail.com>
2023-08-17 17:35:27 +0800

    Add 2nd solution for LeetCode 977 (Square of a sorted array)

--- dsa/sorted_squares.go
+++ dsa/sorted_squares.go
@@ -5,6 +5,28 @@ import (
 	"math/rand"
 )
 
+// SortedSquaresSolution2 makes an O(n) improvements with Two Pointers.
+func SortedSquaresSolution2(nums []int) []int {
+	// These two pointers start and end will traverse the given array start from left and right,
+	// compare which square number is bigger then put it in a new slice in the same order.
+	len := len(nums)
+	start, end, result := 0, len-1, len-1
+	ansSlice := make([]int, len)
+	for start <= end {
+		startValue, endValue := nums[start]*nums[start], nums[end]*nums[end]
+
+		if startValue < endValue {
+			ansSlice[result] = endValue
+			end--
+		} else {
+			ansSlice[result] = startValue
+			start++
+		}
+		result--
+	}
+	return ansSlice
+}
+
 // SortedSquares returns an array of the squares of each number sorted in non-decreasing order.
 //
 // Example 1:
