2862bfa4af3b84f7a82c93db5d09f56870541cd8
Yao Wen <yaowen7902@gmail.com>
2023-08-18 23:44:06 +0800

    Add 1st solution for LeetCode 209 (Minimum size subarray sum)

--- hello/hello.go
+++ hello/hello.go
@@ -16,5 +16,6 @@ func main() {
 	// dsa.MaxProfitSolution3([]int{7, 1, 5, 3, 6, 4})
 	// dsa.BinarySearch([]int{-1, 0, 3, 5, 9, 12}, 9)
 	// dsa.SortedSquares([]int{-4, -1, 0, 3, 10})
-	dsa.SortedSquaresSolution2([]int{-4, -1, 0, 3, 10})
+	// dsa.SortedSquaresSolution2([]int{-4, -1, 0, 3, 10})
+	dsa.MinSubArrayLen([]int{1, 1, 1}, 4)
 }
