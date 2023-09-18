d00ad04ff1e657f4fe0c9adca5140ea47b4c8ba8
Yao Wen <yaowen7902@gmail.com>
2023-09-18 19:06:34 +0800

    Add 1st solution of Reverse String (LeetCode 344)

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	dsa.TwoSumApproach3([]int{2, 3, 5, 7}, 9)
-	// dsa.RemoveDuplicate([]int{1, 1, 2})
-	// dsa.RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
-
 	// dsa.AppendSlices()
 	// fmt.Println(dsa.ReturnNumOfArray([]int{3, 5, 6, 7}))
 
+	/*
+	 * Topic: Array
+	 */
+	// dsa.TwoSumApproach3([]int{2, 3, 5, 7}, 9)
+	// dsa.RemoveDuplicate([]int{1, 1, 2})
+	// dsa.RemoveElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
 	// dsa.MaxProfit([]int{7, 1, 5, 3, 6, 4})
 	// dsa.MaxProfitSolution2([]int{7, 1, 5, 3, 6, 4})
 	// dsa.MaxProfitSolution3([]int{7, 1, 5, 3, 6, 4})
@@ -24,4 +26,9 @@ func main() {
 	// dsa.ProductExceptSelfApproach2([]int{1, 2, 3, 4})
 	// dsa.MaximumSubarray([]int{-2, 2, 5, -11, 6})
 	// dsa.MaximumSubarrayApproach2([]int{-2, 2, 5, -11, 6})
+
+	/*
+	 * Topic: String
+	 */
+	dsa.ReverseString([]byte{0x48, 0x65, 0x6c, 0x6c, 0x6f})
 }
