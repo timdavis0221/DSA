cb0a7fe6d63572d95c71a5f6571707a1dcd07b6a
Yao Wen <yaowen7902@gmail.com>
2023-10-26 15:48:35 +0800

    Update the comments of MinSubArrayLen

--- dsa/array/min_sub_array_len.go
+++ dsa/array/min_sub_array_len.go
@@ -10,28 +10,31 @@ package array
 // Example 3:
 // Input: [1,1,1,1,1,1,1,1], Target: 11, Output: 0
 func MinSubArrayLen(nums []int, target int) int {
-	p1, p2, sum := 0, 0, 0
+	// The fast pointer indicates the end position of the current range of subarray.
+	// The slow pointer indicates the start position of the current range of subarray.
+	slow, fast, sum := 0, 0, 0
 	len := len(nums)
 	// Default minimum length for non-existent cases.
 	defaultMinLen := len + 1
-	// This loop for moving pointer 2 until its length < length of input array in O(n) time.
-	for p2 < len {
-		sum += nums[p2]
-		// This loop only moves pointer 1 when the sum is greater than target.
-		// So, each elements are operated in two times at most.
+	// The number of outer loop depends on the length of input size of nums.
+	for fast < len {
+		sum += nums[fast]
+		// The inner loop only moves the slow pointer when the sum is greater than or equal to target.
+		// Also, each elements are operated in two times at most.
 		for sum >= target {
-			minLen := p2 - p1 + 1
+			minLen := fast - slow + 1
 			if minLen < defaultMinLen {
 				defaultMinLen = minLen
 			}
-			sum -= nums[p1]
-			p1++
+			sum -= nums[slow]
+			// Move the start position forward.
+			slow++
 		}
-		p2++
+		fast++
 	}
 	// There isn't any matched minimum length after going through the iteration.
 	// So indicate the length of input array plus 1 as the condition for non-existent cases.
-	if p2+1 == defaultMinLen {
+	if fast+1 == defaultMinLen {
 		return 0
 	}
 	return defaultMinLen
